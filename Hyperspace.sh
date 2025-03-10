fda81236eb63b8998c5d7ca8fe278788ee6f4863
ljj0522 <76080398+ljj0522@users.noreply.github.com>
2025-03-10 10:40:10 +0800

    Update Hyperspace.sh

--- Hyperspace.sh
+++ Hyperspace.sh
@@ -118,8 +118,8 @@ function deploy_hyperspace_node() {
     echo "当前 PATH: $PATH"
 
     # 提示用户输入私钥并保存为 my.pem 文件
-    echo "请输入你的私钥（按 CTRL+D 结束）："
-    cat > my.pem
+    # echo "请输入你的私钥（按 CTRL+D 结束）："
+    # cat > my.pem
 
     # 使用 my.pem 文件运行 import-keys 命令
     echo "正在使用 my.pem 文件运行 import-keys 命令..."
